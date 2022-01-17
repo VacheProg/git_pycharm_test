0b98358f3de435aaf2cdbd878feb605343047c0a
VacheProg <vache.davtyan.1999@gmail.com>
2022-01-17 18:37:39 +0400

    Initial commit

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def stupid_function(s):
+    print('stupid call')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    stupid_function()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
